e0aebd7f69ac4a5addc68c2ee657d16c394e1cdd
Trevor <trevorpowers22@gmail.com>
2020-07-07 18:32:09 -0500

    clean up make event

--- client/scripts/controllers/makeEvent.controller.js
+++ client/scripts/controllers/makeEvent.controller.js
@@ -1,3 +1,4 @@
+import { _ } from 'meteor/underscore';
 import { Controller } from 'angular-ecmascript/module-helpers';
 import { Chats } from '../../../lib/collections';
 
@@ -16,14 +17,36 @@ export default class MakeEventCtrl extends Controller {
     Chats.remove(chat._id);
   }
 
-  coolstr(){
-    //Chats.insert({ "name" : 'this name', "eventType" : 'social' });
-}
-
 
   myNewEvent(){
-   Chats.insert({ "name" : this.myname, "eventType" : this.myeventType, "summary" : this.mysummary, "description" : this.mydescription, "location" : this.mylocation});
+   
+    if (_.isEmpty(this.myname)) return this.handleEmpty("name");
+    if (_.isEmpty(this.myeventType)) return this.handleEmpty("event type");
+    if (_.isEmpty(this.mysummary)) return this.handleEmpty("event summary");
+    if (_.isEmpty(this.mydescription)) return this.handleEmpty("event description");
+    if (_.isEmpty(this.mylocation)) return this.handleEmpty("event location");
+    this.handleEmpty();
+
+    Chats.insert({ "name" : this.myname, "eventType" : this.myeventType, "summary" : this.mysummary, "description" : this.mydescription, "location" : this.mylocation});
+    delete this.myname
+    delete this.myeventType
+    delete this.mysummary
+    delete this.mylocation
+    delete this.mydescription
+  }
+
+  handleEmpty(myerr){
+   this.$ionicPopup.alert({
+      title: 'Insufficient information provided!',
+      template: 'Please fill in the '  + myerr + ' field',
+      okType: 'button-positive button-clear'
+    }); 
   } 
+
+
 }
 
+//import underscore
+
 MakeEventCtrl.$name = 'MakeEventCtrl';
+MakeEventCtrl.$inject = ['$ionicPopup'];
